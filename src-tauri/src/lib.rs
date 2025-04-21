64ac399b4a29f871b8f61051d83423c7f507574c
Adel2411 <hadjarabadel.2411@gmail.com>
2025-04-21 01:56:27 +0100

    Integrated MiniSoft compiler to the tauri app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,18 +1,25 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use minisoft_compiler::run_compiler;
 use std::time::{SystemTime, UNIX_EPOCH};
+use tauri::command;
 
-#[tauri::command]
+#[command]
 fn greet() -> String {
   let now = SystemTime::now();
   let epoch_ms = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
   format!("Hello world from Rust! Current epoch: {}", epoch_ms)
 }
 
+#[command]
+fn compile_minisoft(file_path: String, verbose: bool) -> Result<String, String> {
+  run_compiler(&file_path, verbose)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_opener::init())
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![greet, compile_minisoft])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
